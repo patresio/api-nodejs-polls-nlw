995c5270f2155055e62018e224a435c48b0ebead
Ricardo Pereira <patresio@gmail.com>
2024-02-08 11:10:08 -0300

    feat: websockets in the realtime, polls

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { prisma } from '../../lib/prisma';
 import { randomUUID } from 'node:crypto';
+import { redis } from '../../lib/redis';
+import { voting } from '../../utils/voting-pub-sub';
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -17,7 +19,9 @@ export async function voteOnPoll(app: FastifyInstance) {
     const { pollId } = voteOnPollParams.parse(request.params)
     const { pollOptionId } = voteOnPollBody.parse(request.body)
 
-    let { sessionId } = request.cookies
+    let { sessionId } = request.cookies;
+
+    console.log(sessionId)
 
     if (sessionId) {
         const userPreviousVoteOnPoll = await prisma.vote.findUnique({
@@ -37,31 +41,46 @@ export async function voteOnPoll(app: FastifyInstance) {
                 }
             })
 
+            const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+            voting.publish(pollId, {
+                pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                votes: Number(votes)
+            })
+
             // Criar um novo
         } else if (userPreviousVoteOnPoll) {
             return reply.status(400).send({ message: 'You already voted on this poll' })
         }
     }
 
-    if (!sessionId){
-        sessionId = randomUUID()
-    
+    if (!sessionId) {
+        sessionId = randomUUID();
+
         reply.setCookie('sessionId', sessionId, {
             path: '/',
             maxAge: 60 * 60 * 24 * 30, // 30 days
             signed: true,
             httpOnly: true,
-        })    
+        });
     }
+
+    console.log('sessionID created: ', sessionId)
 
     await prisma.vote.create({
         data: {
             sessionId,
             pollId, 
             pollOptionId,
-        }
+        },
     })
 
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+        pollOptionId,
+        votes: Number(votes)
+    })
 
     return reply.status(201).send()
 })
--- src/http/server.ts
+++ src/http/server.ts
@@ -3,21 +3,25 @@ import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-polls";
 import { voteOnPoll } from "./routes/vote-on-poll";
 import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-result";
 
 
 const app = fastify()
 
 // GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
 app.register(cookie, {
-    secret: "my-secret",
-    hook: 'onRequest',
-    parseOptions: {}
+  secret: "polls-app-nlw",
+  hook: 'onRequest',
 })
+
+app.register(websocket)
 
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
 
+app.register(pollResults)
 
 app.listen({port:3333}).then(() => {
     console.log('HTTP server running')
